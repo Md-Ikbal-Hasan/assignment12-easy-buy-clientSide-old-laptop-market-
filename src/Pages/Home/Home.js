68345236fbaae5c2f4a481b4f608117d0513bbc9
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-11-24 19:07:59 +0600

    advertised product added in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,52 @@
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+import { FaCheckCircle } from "react-icons/fa";
 const Home = () => {
+    const { data: products } = useLoaderData();
+    console.log(products);
+
     return (
-        <div>
-            <h2 className="text-3xl">This is Home Page</h2>
+        <div className='m-5'>
+
+            {
+                products.length ?
+                    <>
+                        <h2 className="text-3xl">Advertised products</h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                            {
+
+                                products.map(product => {
+                                    return (
+                                        <div key={product._id} className="card  border">
+                                            <figure><img src={product.image} alt="Shoes" /></figure>
+                                            <div className="card-body p-3">
+                                                <h2 className="card-title">  {product.name} </h2>
+                                                <p> <b>Description:</b> {product.description} </p>
+                                                <div className='flex justify-between'>
+                                                    <p> <b>Used:</b> {product.yearOfUsed} year </p>
+                                                    <p> <b>Location:</b> {product.location}</p>
+                                                </div>
+                                                <div className='flex justify-between'>
+                                                    <p> <b>Resell Price:</b> {product.resellPrice} tk </p>
+                                                    <p> <b>Original Price:</b> {product.originalPrice} tk</p>
+                                                </div>
+
+                                                <p className='flex items-center'>
+                                                    <span className='mr-1'><b>Seller:</b> {product.sellerEmail}</span> <FaCheckCircle className='text-blue-600' />
+                                                </p>
+
+                                                <button className='btn btn-primary '>Book Now</button>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+
+                            }
+                        </div>
+                    </>
+                    :
+                    <></>
+            }
         </div>
     );
 };

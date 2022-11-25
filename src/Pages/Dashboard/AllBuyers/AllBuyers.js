cb64be8698e455d8b4fe00be3e7003fdd8d95214
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-11-25 19:41:05 +0600

    seller verified added by admin done

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,62 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Spinner from '../../../components/Spinner';
 
 const AllBuyers = () => {
+
+    const { data: allbuyer = [], isLoading } = useQuery({
+        queryKey: ['allbuyer'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/allbuyer', {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+            const data = res.json();
+            return data;
+        }
+    })
+
+    console.log(allbuyer);
+
+    if (isLoading) {
+        return <Spinner></Spinner>
+    }
+
+
+
     return (
         <div className='m-5'>
             <h2 className='text-3xl'>All Buyers</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allbuyer.length ?
+                                allbuyer.map((buyer, index) => <tr key={buyer._id}>
+                                    <th> {index + 1} </th>
+                                    <td>{buyer.name}</td>
+                                    <td>{buyer.email}</td>
+                                    <td>
+                                        <button className='btn btn-sm btn-error'>Delete</button>
+
+                                    </td>
+                                </tr>)
+                                :
+                                <tr><td className='text-center text-2xl'>Please order some products</td></tr>
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 import Spinner from '../../../components/Spinner';
 
 const AllSellers = () => {
@@ -23,10 +24,75 @@ const AllSellers = () => {
         return <Spinner></Spinner>
     }
 
+    const handleVerifySeller = (id) => {
+        console.log("verify seller id: ", id);
+
+        fetch(`http://localhost:5000/verifyseller/${id}`, {
+            method: "PUT",
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount === 1) {
+                    toast.success("Seller is verified!");
+                    refetch();
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+    }
+
 
     return (
         <div className='m-5'>
-            <h2 className='text-3xl'>All Sellers</h2>
+            <h2 className='text-3xl'>All Sellers : {allseller.length} </h2>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Verify User</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            allseller.length ?
+                                allseller.map((seller, index) => <tr key={seller._id}>
+                                    <th> {index + 1} </th>
+                                    <td>{seller.name}</td>
+                                    <td>{seller.email}</td>
+                                    {
+                                        seller.verified ?
+                                            <td  > <button className='btn btn-sm btn-success'>Verified</button> </td>
+                                            :
+                                            <td onClick={() => handleVerifySeller(seller._id)} > <button className='btn btn-sm btn-primary'>Verify</button> </td>
+
+                                    }
+
+                                    <td>
+                                        <button className='btn btn-sm btn-error'>Delete</button>
+                                    </td>
+                                </tr>)
+
+                                :
+                                <tr><td className='text-center text-2xl'>Please order some products</td></tr>
+
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
